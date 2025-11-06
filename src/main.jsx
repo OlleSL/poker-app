810bac480bdcae9708338449b7c335153b49286d
Olle Sonesson Lidholt <ollesl2002@gmail.com>
2025-11-05 21:15:48 -0500

    page

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./css/GtoPanel.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/poker-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
